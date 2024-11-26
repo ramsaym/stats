6ad9c2bc44d6ae37ad960a7638e9ad9d8c8cdaef
ramsaym <48220108+ramsaym@users.noreply.github.com>
2024-11-26 12:59:38 -0400

    Update init.py

--- init.py
+++ init.py
@@ -32,7 +32,7 @@ for i in range(sampling_rows):
     
     if i % 10000 == 0: print(i)
     
-    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size = 0.2, random_state = i)
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size = 0.2, random_state = i,stratify = y)
     #print(X_train.shape[0])
     sampling_results.loc[i,'seed'] = i
     sampling_results.loc[i,'train_survive'] = sum(y_train == 1) / X_train.shape[0]
@@ -47,5 +47,37 @@ for i in range(sampling_rows):
 
 
 sampling_results.quantile([0, 1])
-print(sampling_results)    
+print(sampling_results.head())    
 sampling_results.to_csv('../python_sampling_results_' + str(sampling_rows) + '.csv', index = False)
+
+sr = pd.read_csv('../python_sampling_results_200000.csv')
+
+# Compute change in survival % between training and validation set
+sr['survive_diff'] = abs(sr.train_survive - sr.val_survive)
+
+# Plot
+
+fig, axs = plt.subplots(1, 1)
+axs.hist(sr.survive_diff, bins = np.arange(0, 0.2, step = 0.01), density = True, edgecolor = 'black', align = 'right')
+
+axs.set_facecolor((242/255, 242/255, 242/255))
+plt.xticks(np.arange(0.02, 0.22, step = 0.02))
+plt.yticks(np.arange(2, 24, step = 2))
+axs.xaxis.set_major_formatter(mtick.PercentFormatter(xmax = 1, decimals = 0))
+axs.yaxis.set_major_formatter(mtick.PercentFormatter(xmax = 100, decimals = 0))
+
+plt.text(x = 0.11, y = 20, s = "Total Data Splits: 200K", fontsize = 13)
+plt.xlabel('Survival % Difference: Training vs. Validation data', fontsize = 13)
+plt.ylabel('% of Data Splits', fontsize = 14)
+plt.style.use(plt.style.available[11])  # 0, 11, 12, 13, 14, 15, 16
+plt.show()
+
+p = sns.distplot(sr.survive_diff, norm_hist = True, bins = 19, kde = False, 
+                 hist_kws = dict(edgecolor = "black"), label = "TEST")
+p.set(xlabel = 'Survival % Difference between Training and Validation data', 
+      ylabel = '% of Data Splits')
+#p.set_xticklabels(np.arange(0, 0.2, step = 0.0))
+#p.set_yticklabels(np.arange(0, 22, step = 2))
+sns.despine()
+#p.xticks(np.arange(0, 0.2, step = 0.02))
+plt.show()
