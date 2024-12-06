9db4f8487585b4d7281f515872dd68a240e87b70
ramsaym <48220108+ramsaym@users.noreply.github.com>
2024-12-06 07:53:45 -0400

    Update rf.py

--- rf.py
+++ rf.py
@@ -8,25 +8,36 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score
 from statistics import mean
 from tqdm import tqdm
+import time
 
 
-def randomforest(X_train,y_train,X_test,y_test):
+def randomforest(X_train,y_train,X_test,y_test,randseed=0):
     # creating a RF classifier
-    clf = RandomForestClassifier(n_estimators = 100)  
-    
+    rf = RandomForestClassifier(random_state=randseed)  
+    feature_names = [f"feature {i}" for i in range(X_test.shape[1])]
     # Training the model on the training dataset
     # fit function is used to train the model using the training sets as parameters
-    clf.fit(X_train, y_train)
-    
+    rf.fit(X_train, y_train)
     # performing predictions on the test dataset
-    y_pred = clf.predict(X_test)
+    y_pred = rf.predict(X_test)
+    feature_important = rf.feature_importances_
 
-    print(clf.feature_importances_)
-    
+    start_time = time.time()
+    importances = rf.feature_importances_
+    std = np.std([tree.feature_importances_ for tree in rf.estimators_], axis=0)
+    elapsed_time = time.time() - start_time
+
+    print(f"Elapsed time to compute the importances: {elapsed_time:.3f} seconds")
+    forest_importances = pd.Series(importances, index=feature_names)
+    print(forest_importances )
+
+    # fig, ax = plt.subplots()
+    # forest_importances.plot.bar(yerr=std, ax=ax)
+    # ax.set_title("Feature importances using MDI")
+    # ax.set_ylabel("Mean decrease in impurity")
+    # fig.tight_layout()
     # metrics are used to find accuracy or error
-    from sklearn import metrics  
-    print()
-    
+    from sklearn import metrics      
     # using metrics module for accuracy calculation
     print("ACCURACY OF THE MODEL:", metrics.accuracy_score(y_test, y_pred))
     #print("Precision OF THE MODEL:", metrics.precision_score(y_test, y_pred))
