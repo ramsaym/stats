f3d9689fac9c54216a64cdb477c47bd0cb01a78d
ramsaym <48220108+ramsaym@users.noreply.github.com>
2024-12-08 09:52:41 -0400

    Update stats.py

--- stats.py
+++ stats.py
@@ -58,7 +58,7 @@ ftrain = train_all[train_all['Crop 1.23_RootC'] > 0]
 
 print(f"---SETTING UP - DROPPING {COL} FROM X DATASET")
 y = ftrain['Crop 1.23_RootC'].astype('int64')
-if FOCUS and columnsofinterest:
+if FOCUS and columnsofinterest is not False:
     #This takes on the columns specified by a list/array. Can be config ported. Split up dependent and independent variables
     X = ftrain[columnsofinterest].drop('Crop 1.23_RootC', axis = 1) 
     
