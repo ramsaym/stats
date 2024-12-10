f392ccdf11895b35796c6ee2f4a9ae66c604f742
ramsaym <48220108+ramsaym@users.noreply.github.com>
2024-12-10 14:05:27 -0400

    Update stats.py

--- stats.py
+++ stats.py
@@ -57,6 +57,7 @@ print(f"----    SETTING UP - DROPPING {COL} FROM X DATASET")
 y = ftrain['Crop 1.23_RootC'].astype('int64')
 if FOCUS =='y':  
     print("!--- FOCUS IS SET") 
+    print(ftrain)
     #X = ftrain[columnsofinterest].drop(columns=excludeColumns) 
     X = dropColumnList(ftrain[columnsofinterest],excludeColumns)
 else:   
