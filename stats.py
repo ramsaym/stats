15f56aa67c6fb788ccd59b50ccb18d45ab9f3188
ramsaym <48220108+ramsaym@users.noreply.github.com>
2024-12-10 14:50:36 -0400

    Update stats.py

--- stats.py
+++ stats.py
@@ -54,7 +54,7 @@ SAMPLE=False
 sampling_results = pd.DataFrame(np.nan, index = range(sampling_rows), columns = ['seed', 'rootc_train', 'rootc_max', 'rootc_val', 'rootc_max'])
 print(f"----    SETTING UP - DROPPING {COL} FROM X DATASET")
 y = ftrain['Crop 1.23_RootC'].astype('int64')
-X = dropColumnList(ftrain,excludeColumns,None)
+X = dropColumnList(ftrain,excludeColumns)
     
     
 
