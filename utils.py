b61073a16fce8efbefc01f591e85fb5c66f85387
ramsaym <48220108+ramsaym@users.noreply.github.com>
2024-12-10 14:21:04 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,9 +5,9 @@ def dropColumnList(df,delCols,keepCols):
     for c in delCols:
         if keepCols is not None:
             if c in keepCols:
-                rdf = df.drop(c,axis=1, inplace=True)
+                rdf = df.drop(c)
         else:
             if c in df.keys().to_list():
-                rdf = df.drop(c,axis=1, inplace=True)
+                rdf = df.drop(c)
 
     return rdf
