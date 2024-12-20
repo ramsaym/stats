e5d4bce3db0d5f9ced51f10877b7357b6b160c63
ramsaym <48220108+ramsaym@users.noreply.github.com>
2024-12-20 19:03:47 -0400

    Update stats.py

--- stats.py
+++ stats.py
@@ -59,7 +59,7 @@ def calculate_variance_entropy(engine, table_name, column_name):
         results_as_dict = resultset.mappings().all()
  
     conn.close()
-    column_data = [item[0] for item in results_as_dict]
+    column_data = [item[0] for item in resultset]
     variance = np.var(column_data)
     # value_counts = np.bincount(column_data)
     # ent = entropy(value_counts)
