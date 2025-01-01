79d744f8ed9fcfcc8510c3df4dc7d61241355f7b
ramsaym <48220108+ramsaym@users.noreply.github.com>
2025-01-01 17:14:25 -0400

    Update statserve.py

--- statserve.py
+++ statserve.py
@@ -201,7 +201,8 @@ SAMPLE=False
 sampling_results = pd.DataFrame(np.nan, index = range(sampling_rows), columns = ['seed', 'rootc_train', 'rootc_max', 'rootc_val', 'rootc_max'])
 print(f"----    SETTING UP - DROPPING {COL} FROM X DATASET")
 y = ftrain[COL].str.strip().fillna('', inplace=True)
-X = dropColumnList(ftrain,excludeColumns).str.strip().astype('float64')
+X = dropColumnList(ftrain,excludeColumns).fillna('', inplace=True)
+#.str.strip().astype('float64')
 ####PROCESS#####################################################
 #################################################################
 print(f"------  ANALYZING PREDICTORS OF {COL}")
